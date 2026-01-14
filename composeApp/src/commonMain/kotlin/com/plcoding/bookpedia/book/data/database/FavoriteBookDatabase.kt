c439ee40f9a387a3ee1eae980a71d43d172c6be9
vignesh-discovery <vignesh.pai@wbdcontractor.com>
2026-01-15 01:39:48 +0530

    fixed ios build

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/database/FavoriteBookDatabase.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/database/FavoriteBookDatabase.kt
@@ -1,5 +1,6 @@
 package com.plcoding.bookpedia.book.data.database
 
+import androidx.room.ConstructedBy
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
@@ -9,6 +10,7 @@ import androidx.room.TypeConverters
     version = 1,
 )
 @TypeConverters(StringListTypeConverter::class)
+@ConstructedBy(BookDatabaseConstructor::class)
 abstract class FavoriteBookDatabase: RoomDatabase() {
     abstract val favoriteBookDao: FavoriteBookDao
 
