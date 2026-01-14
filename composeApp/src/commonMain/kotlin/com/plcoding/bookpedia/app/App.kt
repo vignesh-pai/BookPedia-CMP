61ec03e848c3718f122aa821a5765a88fd601b85
vignesh-discovery <vignesh.pai@wbdcontractor.com>
2026-01-14 16:37:49 +0530

    shared viewmodel added

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/app/App.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/app/App.kt
@@ -5,14 +5,23 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navigation
 import androidx.navigation.toRoute
 import com.plcoding.bookpedia.book.presentation.book_list.BookListScreenRoute
 import com.plcoding.bookpedia.book.presentation.book_list.BookListViewModel
+import com.plcoding.bookpedia.book.presentation.book_list.SelectedBookViewModel
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
@@ -23,25 +32,50 @@ fun App() {
             startDestination = Route.BookGraph,
             navController = navController
         ) {
-            composable<Route.BookGraph> {
-                val viewModel = koinViewModel<BookListViewModel>()
-                BookListScreenRoute(
-                    viewModel = viewModel,
-                    onBookClick = { book ->
-                        navController.navigate(Route.BookDetails(book.id))
+            navigation<Route.BookGraph>(
+                startDestination = Route.BookList
+            ) {
+                composable<Route.BookList> {
+                    val viewModel = koinViewModel<BookListViewModel>()
+                    val selectedBookViewModel = it.shareKoinViewModel<SelectedBookViewModel>(
+                        navController = navController
+                    )
+                    LaunchedEffect(true) {
+                        selectedBookViewModel.onSelectedBook(null)
                     }
-                )
-            }
-            composable<Route.BookDetails> { entry ->
-                val bookId = entry.toRoute<Route.BookDetails>().id
-                Box(
-                    modifier = Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.Center,
-                ) {
-                    Text("Book Details for ID: $bookId")
+                    BookListScreenRoute(
+                        viewModel = viewModel,
+                        onBookClick = { book ->
+                            selectedBookViewModel.onSelectedBook(book)
+                            navController.navigate(Route.BookDetails(book.id))
+                        }
+                    )
+                }
+                composable<Route.BookDetails> {
+                    val selectedBookViewModel = it.shareKoinViewModel<SelectedBookViewModel>(
+                        navController = navController
+                    )
+                    val selectedBook by selectedBookViewModel.selectedBook.collectAsStateWithLifecycle()
+
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        Text("Book Details screen! ${selectedBook?.id}")
+                    }
                 }
             }
         }
-
     }
+}
+
+@Composable
+private inline fun <reified T : ViewModel> NavBackStackEntry.shareKoinViewModel(
+    navController: NavController
+): T {
+    val navGraphRoute = destination.parent?.route ?: return koinViewModel<T>()
+    val parentEntry = remember(this) {
+        navController.getBackStackEntry(navGraphRoute)
+    }
+    return koinViewModel(viewModelStoreOwner = parentEntry)
 }
--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/di/Modules.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/di/Modules.kt
@@ -5,6 +5,7 @@ import com.plcoding.bookpedia.book.data.network.RemoteBookDataSource
 import com.plcoding.bookpedia.book.data.repository.DefaultBookRepository
 import com.plcoding.bookpedia.book.domain.BookRepository
 import com.plcoding.bookpedia.book.presentation.book_list.BookListViewModel
+import com.plcoding.bookpedia.book.presentation.book_list.SelectedBookViewModel
 import com.plcoding.bookpedia.core.data.HttpClientFactory
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.singleOf
@@ -20,4 +21,5 @@ val sharedModules = module {
     singleOf(::DefaultBookRepository).bind<BookRepository>()
 
     viewModelOf(::BookListViewModel)
+    viewModelOf(::SelectedBookViewModel)
 }
