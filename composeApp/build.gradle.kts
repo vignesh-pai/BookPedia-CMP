6c420d75c94e4d48a8eb795cbb0f75b77d924ac9
vignesh-discovery <vignesh.pai@wbdcontractor.com>
2026-01-15 01:53:06 +0530

    database fixed

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -112,6 +112,7 @@ android {
 
 dependencies {
     add("kspCommonMainMetadata", libs.androidx.room.compiler)
+    add("kspDesktop", libs.androidx.room.compiler)
     add("kspAndroid", libs.androidx.room.compiler)
     add("kspIosX64", libs.androidx.room.compiler)
     add("kspIosArm64", libs.androidx.room.compiler)
