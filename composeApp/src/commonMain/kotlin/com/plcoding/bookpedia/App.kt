9df676d9c74ebe692591b84138e0f7c862c05ccd
vignesh-discovery <vignesh.pai@wbdcontractor.com>
2026-01-14 15:29:20 +0530

    http client engine added

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/App.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/App.kt
@@ -3,14 +3,24 @@ package com.plcoding.bookpedia
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
+import com.plcoding.bookpedia.book.data.network.KtorRemoteBookDataSource
+import com.plcoding.bookpedia.book.data.repository.DefaultBookRepository
 import com.plcoding.bookpedia.book.presentation.book_list.BookListScreenRoute
 import com.plcoding.bookpedia.book.presentation.book_list.BookListViewModel
+import com.plcoding.bookpedia.core.data.HttpClientFactory
+import io.ktor.client.engine.HttpClientEngine
 
 @Composable
 @Preview
-fun App() {
+fun App(engine: HttpClientEngine) {
     BookListScreenRoute(
-        viewModel = remember { BookListViewModel() },
+        viewModel = remember { BookListViewModel(
+            repository = DefaultBookRepository(
+                remoteBookDataSource = KtorRemoteBookDataSource(
+                    httpClient = HttpClientFactory.create(engine)
+                )
+            )
+        ) },
         onBookClick = { }
     )
 }
---
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/core/data/HttpClientFactory.kt
@@ -0,0 +1,45 @@
+package com.plcoding.bookpedia.core.data
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+object HttpClientFactory {
+
+    fun create(engine: HttpClientEngine): HttpClient {
+        return HttpClient(engine) {
+            install(ContentNegotiation) {
+                json(
+                    json = Json {
+                        ignoreUnknownKeys = true
+                    }
+                )
+            }
+            install(HttpTimeout) {
+                requestTimeoutMillis = 20_000
+                socketTimeoutMillis = 20_000
+            }
+            install(Logging) {
+                logger = object : Logger {
+                    override fun log(message: String) {
+                        // You can replace this with any logging framework you use
+                        println(message)
+                    }
+                }
+                level = LogLevel.ALL
+            }
+            defaultRequest {
+                contentType(ContentType.Application.Json)
+            }
+        }
+    }
+}
